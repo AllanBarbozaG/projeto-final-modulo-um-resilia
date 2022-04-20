b276054ec556d583355a1b68c9364b0953442b96
allanbarbozag <allpatrick.b@gmail.com>
2022-04-20 01:05:06 -0300

    Adiciona um botão de Decidir! no arquivo hercules-fase-um.html. Implementa o javascript para validação de entrada do usuário com estrutura de repetição e redirecionamento de página com estruturas condicionais.

---
+++ jogo-introducao/hercules/hercules-fase-um/hercules-fase-um.js
@@ -0,0 +1,33 @@
+function validaEntrada() {
+  var decisao = prompt('Digite o número da decisão que deseja tomar (1 ou 2)');
+  while (decisao != '1' && decisao != '2') {
+    alert('Decisão inválida. Digite 1 ou 2')
+    decisao = prompt('Digite o número da decisão que deseja tomar');
+  } if (decisao == '1') {
+    alert('game over!')
+  } else {
+    window.location.assign("/jogo-introducao/hercules/hercules-fase-um/resultado/alternativa-certa/proxima-fase.html");
+    console.log(window.location.href("proxima-fase.html"))
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*function validaEntrada() {
+  var decisao = prompt('Digite o número da decisao que deseja tomar (1 ou 2)');
+  if (decisao != '1' && decisao != '2') {
+    alert('Insira 1 ou 2')
+  } else if (decisao == '1') {
+    alert('game over!')
+  } else {
+    window.location.assign("/jogo-introducao/hercules/hercules-fase-um/resultado/alternativa-certa/proxima-fase.html");
+    console.log(window.location.href("proxima-fase.html"))
+  }
+}*/
