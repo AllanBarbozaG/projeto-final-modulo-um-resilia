60bfe08112057a674f73ab85e6b280cab30c014a
allanbarbozag <allpatrick.b@gmail.com>
2022-04-22 15:17:47 -0300

    Adiciona as pastas dos personagens Hermes e Medusa. Atualiza os arquivos da fases do personagem Hermes com seu conteúdo. Atualiza o link do tão de escolha de personagem do Hermes.

---
+++ medusa/javascript/medusa-fase-dois.js
@@ -0,0 +1,40 @@
+function validaEntrada() {
+  var decisao = prompt('Digite o número da decisão que deseja tomar (1 ou 2)');
+  while(true) {
+    if (decisao == 1 || decisao == 2) {
+      break;
+    } else {
+      alert('Decisão inválida. Digite 1 ou 2');
+      decisao = prompt('Digite o número da decisao que deseja tomar');
+    }
+  } function redirecionaPagina() {
+    if (decisao == '1') {
+      window.location.assign("/hercules/html/hercules-fase-dois-game-over.html");
+    } else {
+      window.location.assign("/hercules/html/hercules-fase-tres.html");
+    }
+  } redirecionaPagina();
+} 
+
+
+/*function validaEntrada() {
+  var decisao = prompt('Digite o número da decisão que deseja tomar (1 ou 2)');
+  while (decisao != '1' && decisao != '2') {
+    alert('Decisão inválida. Digite 1 ou 2')
+    decisao = prompt('Digite o número da decisão que deseja tomar');
+  } if (decisao == '1') {
+    alert('game over!')
+  } else {
+    window.location.assign("/jogo-introducao/hercules/hercules-fase-um/resultado/alternativa-certa/proxima-fase.html");
+  }
+}*/
+
+
+
+
+
+
+
+
+
+
